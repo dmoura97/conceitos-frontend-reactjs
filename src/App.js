2db44c18094bde5928dd9997be2eaa0b7734a46e
Danilo Moura <danilomouraa@icloud.com>
2020-12-22 17:27:26 -0300

    Conceitos front-end com reactJS

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+/**
+ * Componente
+ * Propriedade
+ * Estado & Imutabilidade
+ */
+
+function App() {
+  const [projects, setProjects] = useState(['Desenvolvimente de app', 'Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li> )}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
+}
+
+export default App;
